aa5cc04a4f5f9228448bd3924974f1d173fd0c36
Bossman1 <nikakharadze@yahoo.com>
2025-12-24 00:25:44 +0400

    Add project files.

---
+++ UsersApp/Database.cs
@@ -0,0 +1,50 @@
+﻿using System.Data.SQLite;
+using System.IO;
+
+namespace UsersApp
+{
+    internal static class Database
+    {
+        private static readonly string DbPath = "users_data.db";
+        private static readonly string ConnectionString =  "Data Source=users_data.db;Version=3;";
+
+        public static void Initialize()
+        {
+            if (!File.Exists(DbPath))
+                SQLiteConnection.CreateFile(DbPath);
+
+            using (var con = new SQLiteConnection(ConnectionString))
+            {
+                con.Open();
+
+                using (var cmd = new SQLiteCommand(@"
+                    CREATE TABLE IF NOT EXISTS Users (
+                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        Login TEXT NOT NULL,
+                        Email TEXT NOT NULL,
+                        Password TEXT NOT NULL
+                    );", con))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static void AddUser(User user)
+        {
+            using (var con = new SQLiteConnection(ConnectionString))
+            {
+                con.Open();
+
+                using (var cmd = new SQLiteCommand(
+                    "INSERT INTO Users (Login, Email, Password) VALUES (@l,@e,@p)", con))
+                {
+                    cmd.Parameters.AddWithValue("@l", user.Login);
+                    cmd.Parameters.AddWithValue("@e", user.Email);
+                    cmd.Parameters.AddWithValue("@p", user.Password);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ UsersApp/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SQLite;
+
+namespace UsersApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Database.Initialize();
+        }
+
+        private void Registration_Button(object sender, RoutedEventArgs e)
+        {
+            string Username = TextBoxUsername.Text.Trim();
+            string Email = TextBoxEmail.Text.Trim().ToLower();
+            string Password = TextBoxPassword.Password.Trim();
+            string PasswordAgain = TextBoxPasswordAgain.Password.Trim();
+
+
+            TextBoxUsername.Background = Brushes.White;
+            TextBoxEmail.Background = Brushes.White;
+            TextBoxEmail.Background = Brushes.White;
+            TextBoxPassword.Background = Brushes.White;
+            TextBoxPasswordAgain.Background = Brushes.White;
+
+            bool isError = true;
+
+            if (Username == "")
+            {
+                TextBoxUsername.Background = Brushes.MistyRose;
+                isError = false;
+            }
+
+            if (Password == "")
+            {
+              
+                TextBoxPassword.Background = Brushes.MistyRose;
+                isError = false;
+            }
+
+            if (PasswordAgain == "")
+            {
+                
+                TextBoxPasswordAgain.Background = Brushes.MistyRose;
+                isError = false;
+            }
+
+            if (Username.Length < 3)
+            {
+               
+                TextBoxPassword.Background = Brushes.MistyRose;
+                isError = false;
+            }
+
+            if (Email == "" || !Email.Contains("@") || !Email.Contains("."))
+            {
+                
+                TextBoxEmail.Background = Brushes.MistyRose;
+                isError = false;
+            }
+
+            if (Password != PasswordAgain)
+            {
+                
+                TextBoxPassword.Background = Brushes.MistyRose;
+                isError = false;
+            }
+
+
+            if (isError)
+            {
+
+
+                var user = new User(Username, Email, Password);
+                Database.AddUser(user); 
+              
+
+                MessageBox.Show("Done, User was created");
+               
+            }
+
+        }
+    }
+}
